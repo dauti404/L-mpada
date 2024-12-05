51cfc72332c5f37aa4071252af65f37d92e40ab5
dauti <152911335+dauti404@users.noreply.github.com>
2024-12-04 23:08:08 -0300

    Create Lampada.java
    Class Lampada de main.java

---
+++ Lampada.java
@@ -0,0 +1,12 @@
+class Lampada {
+    String ligar;
+    String desligar;
+
+    public void ligar() {
+        System.out.println("A lâmpada está ligada.");
+    }
+
+    public void desligar() {
+        System.out.println("A lâmpada está desligada.");
+    }
+}
